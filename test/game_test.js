9b9bfe099493ccf81c18e6bbffe4f5c4668f0f76
skatworks <jean_paul_gaultier@msn.com>
2020-10-27 22:48:06 +0900

    スペア、ストライク、パーフェクトのテストを追加。

--- test/game_test.js
+++ test/game_test.js
@@ -5,20 +5,46 @@ var Game = require('../lib/game');
 
 describe("Game", function() {
 
+    beforeEach(function() {
+        game = new Game();
+    });
+    
 	it("gutter game", function() {
-		let game = new Game();
-		for (let i = 0; i < 20; i++) {
-			game.roll(0);
-		}
+        rollMany(20, 0);
 		expect(game.getScore()).to.equal(0);
 	});
 
 	it("all 1pin game", function() {
-		let game = new Game();
-		for (let i = 0; i < 20; i++) {
-			game.roll(1);
-		}
+        rollMany(20, 1);
 		expect(game.getScore()).to.equal(20);
 	});
 
+    it("include spare game", function() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        game.roll(4);
+        rollMany(16, 0);
+        expect(game.getScore()).to.equal(20);
+    });
+
+    it("include strike game", function() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(4);
+        rollMany(17, 0);
+        expect(game.getScore()).to.equal(24);
+    });
+
+    it("perfect game", function() {
+        rollMany(12, 10);
+        expect(game.getScore()).to.equal(300);
+    });
+    
+    function rollMany(n, pins) {
+        for (let i = 0; i < n; i++) {
+            game.roll(pins);
+        }
+    }
+    
 });
